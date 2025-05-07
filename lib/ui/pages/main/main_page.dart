f9e1455e766e6fbac5b401cbac192a25f4192853
dlworua <leegaegyum@gmail.com>
2025-05-07 20:28:45 +0900

    feat: 앱바 하단에 회색 밑줄 추가

--- lib/ui/pages/main/main_page.dart
+++ lib/ui/pages/main/main_page.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('TODO리스트')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TODO 리스트', style: TextStyle(fontWeight: FontWeight.w400)),
+        shape: Border(bottom: BorderSide(color: Colors.grey)),
+      ),
+    );
   }
 }
